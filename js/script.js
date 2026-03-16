f05d5042662dcacc05ae13d9f47ec6a96f8aa190
Stephany <stephanyestevez8@gmail.com>
2026-03-16 15:52:11 +0100

    Actualización 16.03 antes de instalar software

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//////////////////////////////////////////
+    //    * MENU *    //
+$(document).ready(function(){
+    // Toggle menú principal
+    $('.bars, .times').click(function() {
+        $('.menu-movil').toggleClass('open');
+    });
+///////////////////////////////////////////
