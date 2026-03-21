69eb5ab60a05ffee5014d2619243795f916f80ce
Stephany <stephanyestevez8@gmail.com>
2026-03-21 13:05:16 +0100

    video de fondo header

--- js/script.js
+++ js/script.js
@@ -1,9 +1,96 @@
-//////////////////////////////////////////
-    //    * MENU *    //
 $(document).ready(function(){
-    // Toggle menú principal
+    
+    
+//////////////////////////////////////////
+//    * MENU *    //
+// Toggle menú principal
     $('.bars, .times').click(function() {
         $('.menu_movil').toggleClass('open');
     });
+
+//////////////////////////////////////////
+//////////////////////////////////////////
+//    * BOTON ARRIBA *    //
+    $(".boton_arriba").fadeOut();
+        $(document).scroll(function(){
+            if($(document).scrollTop()>200){
+                $(".boton_arriba").fadeIn(1000);
+            } else if ($(document).scrollTop()==0){
+                $(".boton_arriba").fadeOut(1000)
+            };
+        });
+        $(".boton_arriba").click(function(){
+            $("html").animate({scrollTop:0},
+        500);
+    });
+//////////////////////////////////////////
+//////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
-///////////////////////////////////////////
